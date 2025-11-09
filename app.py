bdea97e9b1c04a0fa83e8b6a968ed467d20a06a4
sanjana-ps12 <sanjanaps2005@gmail.com>
2025-11-10 00:36:39 +0530

    upd commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('Home.html')
+
+@app.route('/about')
+def about():
+  return render_template('About.html')
+
+@app.route('/project')
+def project():
+  return render_template('Project.html')
+
+@app.route('/p1')
+def p1():
+  return render_template('p1.html')
+
+@app.route('/p2')
+def p2():
+  return render_template('p2.html')
+
+@app.route('/p3')
+def p3():
+  return render_template('p3.html')
+
+
+if __name__ == '__main__':
+  app.run(debug = True)
